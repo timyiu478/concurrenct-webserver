be9eadd2d34dcc56803627af90306463784289b4
tim <ctom3560@gmail.com>
2025-06-04 23:35:57 +0800

    support mult-threading

--- concurrency-webserver/src/wserver.c
+++ concurrency-webserver/src/wserver.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include "request.h"
 #include "io_helper.h"
+#include "pthread.h"
 
-#define MAXBUF (20)
+#define MAXBUF (50)
 
 char default_root[] = ".";
 
@@ -10,6 +11,10 @@ int conn_fd_buffer[MAXBUF];
 int fill_ptr = 0;
 int use_ptr = 0;
 int conn_fd_counter = 0;
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t fill = PTHREAD_COND_INITIALIZER;
+pthread_cond_t empty = PTHREAD_COND_INITIALIZER;
 
 void put_conn_fd(int conn_fd) {
     conn_fd_buffer[fill_ptr] = conn_fd;
@@ -27,6 +32,20 @@ int get_conn_fd() {
     return fd;
 }
 
+void *request_worker(void *arg) {
+  while (1) {
+    pthread_mutex_lock(&mutex); 
+    while (conn_fd_counter == 0) 
+      pthread_cond_wait(&fill, &mutex);
+    int conn_fd = get_conn_fd();
+    pthread_cond_signal(&empty);
+    pthread_mutex_unlock(&mutex);
+
+    request_handle(conn_fd);
+    close_or_die(conn_fd);
+  }
+}
+
 //
 // ./wserver [-d <basedir>] [-p <portnum>] [-t threads]
 // 
@@ -37,7 +56,7 @@ int main(int argc, char *argv[]) {
     int threads = 1;
     
     // parse command line options
-    while ((c = getopt(argc, argv, "d:p:")) != -1) {
+    while ((c = getopt(argc, argv, "d:p:t:")) != -1) {
       switch (c) {
       case 'd':
           root_dir = optarg;
@@ -54,6 +73,15 @@ int main(int argc, char *argv[]) {
 	    }
     }
 
+    // run the woker threads
+    pthread_t worker_threads[threads];
+    for (int i = 0; i < threads; i++) {
+      if (pthread_create(&worker_threads[i], NULL, request_worker, NULL) != 0) {
+        perror("pthread_create");
+        exit(1);
+      }
+    }
+
     // run out of this directory
     chdir_or_die(root_dir);
 
@@ -63,8 +91,12 @@ int main(int argc, char *argv[]) {
       struct sockaddr_in client_addr;
       int client_len = sizeof(client_addr);
       int conn_fd = accept_or_die(listen_fd, (sockaddr_t *) &client_addr, (socklen_t *) &client_len);
-      request_handle(conn_fd);
-      close_or_die(conn_fd);
+      pthread_mutex_lock(&mutex); 
+      while (conn_fd_counter == MAXBUF) 
+        pthread_cond_wait(&empty, &mutex);
+      put_conn_fd(conn_fd);
+      pthread_cond_signal(&fill);
+      pthread_mutex_unlock(&mutex);
     }
 
     return 0;
